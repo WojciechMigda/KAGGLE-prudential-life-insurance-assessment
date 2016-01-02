b2659236388609fd54698a61e4bb226bcaa85cf0
Wojciech Migda <wojtek.golf@interia.pl>
2016-01-02 17:19:54 +0100

    initial toycode

---
+++ src/toycode.py
@@ -0,0 +1,154 @@
+#!/opt/anaconda2/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+################################################################################
+#
+#  Copyright (c) 2015 Wojciech Migda
+#  All rights reserved
+#  Distributed under the terms of the MIT license
+#
+################################################################################
+#
+#  Filename: toycode.py
+#
+#  Decription:
+#      Toycode
+#
+#  Authors:
+#       Wojciech Migda
+#
+################################################################################
+#
+#  History:
+#  --------
+#  Date         Who  Ticket     Description
+#  ----------   ---  ---------  ------------------------------------------------
+#  2016-01-02   wm              Initial version
+#
+################################################################################
+"""
+
+from __future__ import print_function
+
+
+DEBUG = False
+__all__ = []
+__version__ = 0.1
+__date__ = '2016-01-02'
+__updated__ = '2016-01-02'
+
+from sys import path as sys_path
+sys_path.insert(0, './mlpipes')
+sys_path.insert(0, './Pipe')
+import pipe as P
+
+
+def work():
+
+    from pypipes import as_csv_rows,unzip
+    from nppipes import as_array
+    from csv import QUOTE_NONNUMERIC as CSV_QUOTE_NONNUMERIC
+
+    foo = (
+        '../../data/sample_submission.csv.zip'
+        | unzip('sample_submission.csv')
+        | as_csv_rows(quoting=CSV_QUOTE_NONNUMERIC)
+        | P.skip(1)
+        | P.take(5)
+        #| P.as_list
+        | as_array
+        )
+    print(foo)
+
+    pass
+
+
+def main(argv=None): # IGNORE:C0111
+    '''Command line options.'''
+    from sys import argv as Argv
+
+    if argv is None:
+        argv = Argv
+        pass
+    else:
+        Argv.extend(argv)
+        pass
+
+    from os.path import basename
+    program_name = basename(Argv[0])
+    program_version = "v%s" % __version__
+    program_build_date = str(__updated__)
+    program_version_message = '%%(prog)s %s (%s)' % (program_version, program_build_date)
+    program_shortdesc = __import__('__main__').__doc__.split("\n")[1]
+    program_license = '''%s
+
+  Created by Wojciech Migda on %s.
+  Copyright 2016 Wojciech Migda. All rights reserved.
+
+  Licensed under the MIT License
+
+  Distributed on an "AS IS" basis without warranties
+  or conditions of any kind, either express or implied.
+
+USAGE
+''' % (program_shortdesc, str(__date__))
+
+    try:
+        from argparse import ArgumentParser
+        from argparse import RawDescriptionHelpFormatter
+        from argparse import FileType
+        from sys import stdout,stdin
+
+        # Setup argument parser
+        parser = ArgumentParser(description=program_license, formatter_class=RawDescriptionHelpFormatter)
+        """
+        parser.add_argument("-i", "--in-csv",
+            action='store', dest="in_csv_file", default=stdin,
+            type=FileType('r'),
+            help="input CSV file name")
+        parser.add_argument("-o", "--out-csv",
+            action='store', dest="out_csv_file", default=stdout,
+            type=FileType('w'),
+            help="output CSV file name")
+        parser.add_argument("-e", "--epsilon",
+            type=float, default=2.1, action='store', dest="epsilon",
+            help="epsilon for DBSCAN")
+        """
+
+        # Process arguments
+        args = parser.parse_args()
+
+        for k, v in args.__dict__.items():
+            print(str(k) + ' => ' + str(v))
+            pass
+
+
+        work()
+
+
+        return 0
+    except KeyboardInterrupt:
+        ### handle keyboard interrupt ###
+        return 0
+    except Exception as e:
+        if DEBUG:
+            raise(e)
+            pass
+        indent = len(program_name) * " "
+        from sys import stderr
+        stderr.write(program_name + ": " + repr(e) + "\n")
+        stderr.write(indent + "  for help use --help")
+        return 2
+
+    pass
+
+
+if __name__ == "__main__":
+    if DEBUG:
+        from sys import argv
+        argv.append("-h")
+        pass
+    from sys import exit as Exit
+    Exit(main())
+    pass
