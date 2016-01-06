1d6f38bd4c727f1a9a6360f6bec50685294ed537
Wojciech Migda <wojtek.golf@interia.pl>
2016-01-06 21:11:29 +0100

    label encoding proper part of test data

--- src/toycode.py
+++ src/toycode.py
@@ -49,7 +49,7 @@ def work():
     from pypipes import unzip,as_key,del_key,getitem,setitem
     from nppipes import genfromtxt
     from nppipes import place,astype,as_columns,label_encoder,fit_transform
-    from nppipes import dstack
+    from nppipes import dstack,transform
     from nppipes import take as np_take
     from numpy.core.defchararray import strip
     from numpy import s_
@@ -102,11 +102,16 @@ int     Response
 
 
     missing_cidx = [11, 14, 16, 28, 33, 34, 35, 36, 37, 46, 51, 60, 68]
-    nominal_cidx = [0, 1, 2, 4, 5, 6, 12, 13, 15, 17, 18, 19, 20, 21, 22, 23,
-                 24, 25, 26, 27, 29, 30, 31, 32, 38, 39, 40, 41, 42, 43, 44, 45,
+#    nominal_cidx = [0, 1, 2, 4, 5, 6, 12, 13, 15, 17, 18, 19, 20, 21, 22, 23,
+#                 24, 25, 26, 27, 29, 30, 31, 32, 38, 39, 40, 41, 42, 43, 44, 45,
+#                 47, 48, 49, 50, 52, 53, 54, 55, 56, 57, 58, 59,
+#                 61, 62, 63, 64, 65, 66, 67, 69, 70, 71, 72, 73, 74, 75, 76, 77]
+    unseed_nominal_cidx = [2, 12, 38, 69, 74]
+    seen_nominal_cidx = [0, 1, 4, 5, 6, 13, 15, 17, 18, 19, 20, 21, 22, 23,
+                 24, 25, 26, 27, 29, 30, 31, 32, 39, 40, 41, 42, 43, 44, 45,
                  47, 48, 49, 50, 52, 53, 54, 55, 56, 57, 58, 59,
-                 61, 62, 63, 64, 65, 66, 67, 69, 70, 71, 72, 73, 74, 75, 76, 77]
-
+                 61, 62, 63, 64, 65, 66, 67, 70, 71, 72, 73, 75, 76, 77]
+    nominal_cidx = seen_nominal_cidx + unseed_nominal_cidx
 
     data = (
         '../../data/train.csv.zip'
@@ -161,6 +166,16 @@ int     Response
                 | P.first
                 )
 
+        | as_key('test_X', lambda d:
+                (d['test_X'],)
+                | np_take(seen_nominal_cidx, axis=1)
+                | as_columns
+                | transform(d['label_encoders'][:-len(unseed_nominal_cidx)])
+                | dstack
+                | setitem(d['test_X'].copy(), s_[:, seen_nominal_cidx])
+                | P.first
+                )
+
         | P.first
         )
 
@@ -180,6 +195,8 @@ int     Response
 
     from sklearn.preprocessing import LabelEncoder
     le = LabelEncoder()
+    X_train = data['train_X']
+    X_test = data['test_X']
 
     for fidx in [1, 2, 5, 6, 7, 14, 16, 18, 19, 20, 21, 22, 23, 24,
                  25, 26, 27, 28, 30, 31, 32, 33, 40, 41, 42, 43, 44, 45, 46,
