0fb89f986a796d719abc501c6a23fa79ba3011d3
Wojciech Migda <wojtek.golf@interia.pl>
2016-01-26 21:10:43 +0100

    much faster kappa

--- src/skll.py
+++ src/skll.py
@@ -1,5 +1,5 @@
 """
-Taken and adopted from skll
+kappa taken and adopted from skll
 https://raw.githubusercontent.com/EducationalTestingService/skll/5ea61b8dfc23570e661468457a262b6c2242daa9/skll/metrics.py
 """
 
@@ -14,8 +14,119 @@ evaluate the performance of learners.
 :organization: ETS
 """
 
+"""
+confusion_matrix taken and adopted from sklearn
+https://raw.githubusercontent.com/scikit-learn/scikit-learn/master/sklearn/metrics/classification.py
+"""
 
-def kappa(y_true, y_pred, weights=None, allow_off_by_one=False):
+"""Metrics to assess performance on classification task given class prediction
+
+Functions named as ``*_score`` return a scalar value to maximize: the higher
+the better
+
+Function named as ``*_error`` or ``*_loss`` return a scalar value to minimize:
+the lower the better
+"""
+
+# Authors: Alexandre Gramfort <alexandre.gramfort@inria.fr>
+#          Mathieu Blondel <mathieu@mblondel.org>
+#          Olivier Grisel <olivier.grisel@ensta.org>
+#          Arnaud Joly <a.joly@ulg.ac.be>
+#          Jochen Wersdorfer <jochen@wersdoerfer.de>
+#          Lars Buitinck <L.J.Buitinck@uva.nl>
+#          Joel Nothman <joel.nothman@gmail.com>
+#          Noel Dawe <noel@dawe.me>
+#          Jatin Shah <jatindshah@gmail.com>
+#          Saurabh Jha <saurabh.jhaa@gmail.com>
+#          Bernardo Stein <bernardovstein@gmail.com>
+# License: BSD 3 clause
+
+def confusion_matrix(y_true, y_pred, labels=None):
+    """Compute confusion matrix to evaluate the accuracy of a classification
+
+    By definition a confusion matrix :math:`C` is such that :math:`C_{i, j}`
+    is equal to the number of observations known to be in group :math:`i` but
+    predicted to be in group :math:`j`.
+
+    Read more in the :ref:`User Guide <confusion_matrix>`.
+
+    Parameters
+    ----------
+    y_true : array, shape = [n_samples]
+        Ground truth (correct) target values.
+
+    y_pred : array, shape = [n_samples]
+        Estimated targets as returned by a classifier.
+
+    labels : array, shape = [n_classes], optional
+        List of labels to index the matrix. This may be used to reorder
+        or select a subset of labels.
+        If none is given, those that appear at least once
+        in ``y_true`` or ``y_pred`` are used in sorted order.
+
+    Returns
+    -------
+    C : array, shape = [n_classes, n_classes]
+        Confusion matrix
+
+    References
+    ----------
+    .. [1] `Wikipedia entry for the Confusion matrix
+           <http://en.wikipedia.org/wiki/Confusion_matrix>`_
+
+    Examples
+    --------
+    >>> from sklearn.metrics import confusion_matrix
+    >>> y_true = [2, 0, 2, 2, 0, 1]
+    >>> y_pred = [0, 0, 2, 2, 0, 2]
+    >>> confusion_matrix(y_true, y_pred)
+    array([[2, 0, 0],
+           [0, 0, 1],
+           [1, 0, 2]])
+
+    >>> y_true = ["cat", "ant", "cat", "cat", "ant", "bird"]
+    >>> y_pred = ["ant", "ant", "cat", "cat", "ant", "cat"]
+    >>> confusion_matrix(y_true, y_pred, labels=["ant", "bird", "cat"])
+    array([[2, 0, 0],
+           [0, 0, 1],
+           [1, 0, 2]])
+
+    """
+    import numpy as np
+    from sklearn.utils.multiclass import unique_labels
+    from scipy.sparse import coo_matrix
+
+    #y_type, y_true, y_pred = _check_targets(y_true, y_pred)
+    #if y_type not in ("binary", "multiclass"):
+    #    raise ValueError("%s is not supported" % y_type)
+
+    if labels is None:
+        labels = unique_labels(y_true, y_pred)
+    else:
+        labels = np.asarray(labels)
+
+    n_labels = labels.size
+    if not np.array_equal(labels, range(n_labels)):
+        label_to_ind = dict((y, x) for x, y in enumerate(labels))
+        # convert yt, yp into index
+        y_pred = np.array([label_to_ind.get(x, n_labels + 1) for x in y_pred])
+        y_true = np.array([label_to_ind.get(x, n_labels + 1) for x in y_true])
+
+    # intersect y_pred, y_true with labels, eliminate items not in labels
+    ind = np.logical_and(y_pred < n_labels, y_true < n_labels)
+    y_pred = y_pred[ind]
+    y_true = y_true[ind]
+
+    CM = coo_matrix((np.ones(y_true.shape[0], dtype=np.int), (y_true, y_pred)),
+                    shape=(n_labels, n_labels)
+                    ).toarray()
+
+    return CM
+
+
+def kappa(y_true, y_pred, weights=None, allow_off_by_one=False,
+          min_rating=None,
+          max_rating=None):
     """
     Calculates the kappa inter-rater agreement between two the gold standard
     and the predicted ratings. Potential values range from -1 (representing
@@ -58,44 +169,26 @@ def kappa(y_true, y_pred, weights=None, allow_off_by_one=False):
     from numpy import empty as np_empty
     from numpy import bincount,outer,count_nonzero
 
-#    import logging
-#    logger = logging.getLogger(__name__)
-
     # Ensure that the lists are both the same length
     assert(len(y_true) == len(y_pred))
 
-    # This rather crazy looking typecast is intended to work as follows:
-    # If an input is an int, the operations will have no effect.
-    # If it is a float, it will be rounded and then converted to an int
-    # because the ml_metrics package requires ints.
-    # If it is a str like "1", then it will be converted to a (rounded) int.
-    # If it is a str that can't be typecast, then the user is
-    # given a hopefully useful error message.
-    # Note: numpy and python 3.3 use bankers' rounding.
-#    try:
-#        y_true = [int(np_round(float(y))) for y in y_true]
-#        y_pred = [int(np_round(float(y))) for y in y_pred]
-#    except ValueError as e:
-#        logger.error("For kappa, the labels should be integers or strings "
-#                     "that can be converted to ints (E.g., '4.0' or '3').")
-#        raise e
     y_true = np_round(y_true).astype(int)
     y_pred = np_round(y_pred).astype(int)
 
     # Figure out normalized expected values
-    min_rating = min(min(y_true), min(y_pred))
-    max_rating = max(max(y_true), max(y_pred))
+    if min_rating is None:
+        min_rating = min(min(y_true), min(y_pred))
+    if max_rating is None:
+        max_rating = max(max(y_true), max(y_pred))
 
     # shift the values so that the lowest value is 0
     # (to support scales that include negative values)
-#    y_true = [y - min_rating for y in y_true]
-#    y_pred = [y - min_rating for y in y_pred]
     y_true = y_true - min_rating
     y_pred = y_pred - min_rating
 
     # Build the observed/confusion matrix
     num_ratings = max_rating - min_rating + 1
-    from sklearn.metrics import confusion_matrix
+    #from sklearn.metrics import confusion_matrix
     observed = confusion_matrix(y_true, y_pred,
                                 labels=list(range(num_ratings)))
     num_scored_items = float(len(y_true))
